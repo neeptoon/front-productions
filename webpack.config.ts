de70c38100bdfaeb829166b41bbe2c273bd02212
Evgeniy Shekhanov <eshekhanov@phoenixit.ru>
2023-01-30 22:03:12 +0300

    добавляет конфигурацию webpack

---
+++ webpack.config.ts
@@ -0,0 +1,22 @@
+import path from 'path';
+import webpack from 'webpack';
+import { buildWebpackConfig } from './config/build/buildWebpackConfig';
+import { BuildPaths } from './config/build/types/config';
+
+const paths: BuildPaths = {
+    build: path.resolve(__dirname, 'build'),
+    entry: path.resolve(__dirname, 'src', 'index.ts'),
+    html: path.resolve(__dirname, 'public', 'index.html'),
+}
+
+const mode = 'development';
+const isDev = mode === 'development'
+
+const config: webpack.Configuration = buildWebpackConfig({
+    mode: 'development',
+    paths,
+    isDev,
+});
+
+
+export default config;
