eb78f3ba0b3ea1ccce7d9d0ff1d27f77dd5d2baa
Evgeniy Shekhanov <eshekhanov@phoenixit.ru>
2023-02-01 08:47:00 +0300

    добавляет css modules

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
-import './Counter.scss'
+import classes from  './Counter.module.scss';
 
 export const Counter = () => {
     const [count, setCounter] = useState(0);
     return (
         <>
-            <h1>{count}</h1>
-            <button onClick={() => setCounter(count + 1)}>increment</button>
+            <h1 className={classes.upper}>{count}</h1>
+            <button className={classes.upper} onClick={() => setCounter(count + 1)}>increment</button>
         </>
     )
 }
