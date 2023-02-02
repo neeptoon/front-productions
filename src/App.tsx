278914fbac938a5f925eeaf1c8374b657bc94f21
Evgeniy Shekhanov <eshekhanov@phoenixit.ru>
2023-02-02 13:15:00 +0300

    добавляет хелпер для работы с калссами

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import AboutPageAsync from './pages/AboutPage/AboutPageAsync';
 import MainPageAsync from "./pages/MainPage/MainPageAsync";
 import { Suspense} from "react";
 import { useTheme } from "./theme/useTheme";
+import { classNames } from './helpers/classNames/classNames';
 
 enum Theme {
     LIGHT = 'light',
@@ -14,7 +15,7 @@ const App = () => {
 
     const {theme, toggleTheme} = useTheme();
     return (
-        <div className={`app ${theme}`}>
+        <div className={classNames('app', [theme], {})}>
             <Link to={'/about'}>О сайте</Link>
             <Link to={'/'}>Главная страница</Link>
             <button onClick={toggleTheme}>Change Theme</button>
